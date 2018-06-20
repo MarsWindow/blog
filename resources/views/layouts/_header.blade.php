6e0f0f979496c19e062ada5d59d5a878a57294e9
bullsli <xylilei19911117@163.com>
2018-06-20 09:23:54 +0000

    List topics created by user

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -50,6 +50,12 @@
                             {{ Auth::user()->name }} <span class="caret"></span></a>
                         <ul class="dropdown-menu">
                             <li>
+                                <a href="{{ route('users.show', Auth::id()) }}">
+                                    <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                                    个人中心
+                                </a>
+                            </li>
+                            <li>
                                 <a href="{{ route('users.edit', Auth::id()) }}">
                                     编辑资料
                                 </a>
