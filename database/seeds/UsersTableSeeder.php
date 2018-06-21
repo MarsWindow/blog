c1c3ef7c8cbc729177a024b0b07d302555e87414
bullsli <xylilei19911117@163.com>
2018-06-21 08:54:54 +0000

    Integrate laravel-permission package

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -48,5 +48,12 @@ class UsersTableSeeder extends Seeder
         $user->email = '928974955@qq.com';
         $user->avatar = 'http://leoblog.test/uploads/images/avatars/201806/20/2_1529466318_EpTZWqxW7V.jpg';
         $user->save();
+
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
     }
 }
